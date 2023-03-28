ea56469750041910811f977486cb54ed41dd9de4
khb6997@gmail.com <khb6997@gmail.com>
2023-03-28 10:08:02 +0900

    HelloController 추가

--- src/main/java/web/HelloController.java
+++ src/main/java/web/HelloController.java
@@ -1,5 +1,13 @@
 package web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
 
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "hello world!";
+    }
 }
